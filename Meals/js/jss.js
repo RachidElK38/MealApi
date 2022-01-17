cdcd5c6db534aba54ee9d79fe66780ae7adc4a75
Rachid ElKhamlichi <92023649+RachidElK38@users.noreply.github.com>
2022-01-17 01:28:08 +0100

    Update jss.js

--- Meals/js/jss.js
+++ Meals/js/jss.js
@@ -1,63 +1,55 @@
-var search = document.getElementById("search");
+var inputSearch = document.getElementById("inputSearch");
 var submit = document.getElementById("submit");
-var random =document.getElementById("random");
 var mealEl = document.getElementById("meals");
-var result = document.getElementsByClassName("result");
 var cat = document.querySelector("#categories");
-
-
-const mealDetailsContent = document.querySelector('.meal-details-content');
-
-mealEl.addEventListener('click',getMealRecipe)
-
-
 
 var api = "https://www.themealdb.com/api/json/v1/1/";
 
-// function searchMeal(e){
-//     e.preventDefault();
+async function searchMeal(e){
+        e.preventDefault();
+        if(inputSearch.value.trim()){
+            
+                var res = await fetch(`${api}search.php?s=${inputSearch.value}`);
+                var data = await res.json();
+                console.log(data);
+                mealEl.innerHTML = data.meals.map((meal)=>`
+                <div class="col-6 col-md-4 col-lg-3 my-4" id="meal"">
+                        <div class="card h-100  bg-dark ">
+                        <img src="${meal.strMealThumb}"alt="${meal.strMeal}" class="card-img-top" />
+                        <div >
+                        <h6 class="text-white my-4 mx-1">
+                           ${meal.strMeal}
+                        </h6>   
+                        <h6 class="text-white"> <strong>Cat</strong> ${meal.strCategory} </h6> 
+                        <h6 class="text-white" > ${meal.strArea} </h6>
+                      <h6  class="text-white"> ${meal.strTags} </h6>
+                      </div>
+                        </div>
+                    </div>
+                `).join("");  }         }
 
-//     if(search.value.trim()){
-//         fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search.value}`)
-//         .then((res)=>res.json())
-//         .then((data)=>{
-//             result.innerHTML = `<h2>${search.value}</h2>`
-//             if(data.meals===null){
-//                 result.innerHTML =`<h2>No result ${search.value}</h2>`
-//             }
-//             else
-//             {
-//                 mealEl.innerHTML = data.meals.map(
-//                     (meal)=>`
-//                     <div class="meal col-6 col-md-4 col-lg-3 my-5" id="meal">
-//                     <img src="${meal.strMealThumb}"alt="${meal.strMeal}"/>
-//                     <div class="meal-info" data-mealID="${meal.idMeal}"
-//                     <h3>${meal.strMeal}</h3>
-//                      </div> </div> `
-//                 ).join("");
-//             }
-//         })
-//     }
-// }
+submit.addEventListener('submit',searchMeal);        
 async function loadOn()
 {
       var res =  await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=a`);
       var data = await res.json();
-            result.innerHTML = `<h2>All Meals</h2>`
-            
-                mealEl.innerHTML = data.meals.map(
-                    (meal)=>`
-                    <div class="col-6 col-md-4 col-lg-3 my-5" id="meal" data-mealID="${meal.idMeal}">
-                    <img src="${meal.strMealThumb}"alt="${meal.strMeal}"/>
-                    <div class="meal-info" >
-                    <h3>${meal.strMeal}</h3>
-                     </div>
-                     <a href = "#" class = "recipe-btn">Get Recipe</a>
-                      </div> `
-                ).join("");
-}
+        
+                mealEl.innerHTML = data.meals.map((meal)=>`
+                <div class="col-6 col-md-4 col-lg-3 my-4" id="meal"">
+                <div class="card h-100  bg-dark ">
+                <img src="${meal.strMealThumb}"alt="${meal.strMeal}" class="card-img-top" />
+                <div >
+                <h6 class="text-white my-4 mx-1">
+                   ${meal.strMeal}
+                </h6>   
+                <h6 class="text-white"> <strong>Cat</strong> ${meal.strCategory} </h6> 
+                <h6 class="text-white" > ${meal.strArea} </h6>
+              <h6  class="text-white"> ${meal.strTags} </h6>
+              </div>
+                </div>
+            </div>  `).join("");}
 /*Fill comboBox*/    
-async function searchM(){
+async function fillSelectCategories (){
     var res = await fetch(`https://www.themealdb.com/api/json/v1/1/categories.php`)
     var data = await res.json();
     cat.innerHTML = ` <select onchange="getCat(this.value)">
@@ -66,54 +58,32 @@ async function searchM(){
 }
 
 loadOn();
-searchM();
+fillSelectCategories();
 async function getCat(categorie){
     var res = await fetch(`${api}filter.php?c=${categorie}`);
     var data = await res.json();
-   console.log(data);
+   console.log(data.meals);
+
    mealEl.innerHTML = data.meals.map(
-    (meal)=>`
-        <div class="meal col-6 col-md-4 col-lg-3 my-5" id="meal">
-            <img src="${meal.strMealThumb}"alt="${meal.strMeal}"/>
-            <div class="meal-info" data-mealID="${meal.idMeal}"
-                 <h3>${meal.strMeal}</h3>
-            </div> 
-        </div> `
-).join("");
-}
-
-async function getMealRecipe(e){
-    e.preventDefault();
-    console.log(e.target.parentElement)
-    if(e.target.classList.contains('recipe-btn')){
-        let mealItem = e.target.parentElement;
-        var res =  await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`);
-      var data = await res.json();
-        mealRecipeModal(data.meals);
-    }
-}
-
-
-function mealRecipeModal(meal){
-    console.log(meal);
-    meal = meal[0];
-    let html = `
-        <h2 class = "recipe-title">${meal.strMeal}</h2>
-        <p class = "recipe-category">${meal.strCategory}</p>
-        <div class = "recipe-instruct">
-            <h3>Instructions:</h3>
-            <p>${meal.strInstructions}</p>
+  async function gg(meal){
+    var res2 = await fetch(`${api}lookup.php?i=${meal.idMeal}`);
+    var data2 = await res2.json();
+    console.log(data2.meals);
+    
+    mealEl.innerHTML += data2.meals.map(
+        (meal1)=>`
+        <div class="col-6 col-md-4 col-lg-3 my-4" id="meal"">
+        <div class="card h-100  bg-dark ">
+        <img src="${meal1.strMealThumb}"alt="${meal1.strMeal}" class="card-img-top" />
+        <div > 
+        
+        <h6 class="text-white my-4 mx-1">
+           ${meal1.strMeal}
+        </h6> 
+        <h6 class="text-white"> <strong>Cat</strong> ${meal1.strCategory} </h6> 
+                    <h6 class="text-white" > ${meal1.strArea} </h6>
+                  <h6  class="text-white"> ${meal1.strTags} </h6>  
+      </div>
+    
         </div>
-        <div class = "recipe-meal-img">
-            <img src = "${meal.strMealThumb}" alt = "">
-        </div>
-        <div class = "recipe-link">
-            <a href = "${meal.strYoutube}" target = "_blank">Watch Video</a>
-        </div>
-    `;
-    mealDetailsContent.innerHTML = html;
-    mealDetailsContent.parentElement.classList.add('showRecipe');
-}
-//call functions
-
-
+    </div> `  ).join("");}).join("");}
